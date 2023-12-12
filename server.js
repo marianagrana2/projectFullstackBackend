a9a7b060f2e1478e7d2cd5c58f77aed5690fc2de
marianagrana2 <mariana.grana2@gmail.com>
2023-12-11 18:46:26 -0600

    ✨ Database conectada listo

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const dotenv = require("dotenv").config()
+const connectDB = require("./config/database")
 const port = process.env.PORT || 3005
 
+connectDB()
 const app = express();
 
 app.use(express.json())
