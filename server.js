d96221bf05952d69f6494ad51d33a77a4781b1cb
marianagrana2 <mariana.grana2@gmail.com>
2023-12-14 15:41:12 -0600

    ✨ Error middleware listo

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv").config()
 const connectDB = require("./config/database")
+const {errorHandler} = require('./middlewares/errorMiddleware')
 const port = process.env.PORT || 3005
 
 connectDB()
@@ -11,5 +12,5 @@ app.use(express.urlencoded({extended: false}))
 
 app.use("/api/v1/users", require("./routes/usersRoutes"))
 
-
+app.use(errorHandler)
 app.listen(port, () => console.log(`Servidor iniciado en el puerto ${port}`))
